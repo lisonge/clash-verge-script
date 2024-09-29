ab06a453952caabf89bdec62fca73ab580dc51e5
lisonge <i@songe.li>
2024-09-29 21:59:42 +0800

    feat: dns

--- src/index.ts
+++ src/index.ts
@@ -20,26 +20,58 @@ const innerProxyRules = __INNER_PROXY_DOMAIN__
   .map((v) => ['DOMAIN-SUFFIX,' + v + ',' + route.innerProxy])
   .flat();
 
+// 国内DNS服务器
+const domesticNameservers = [
+  'https://dns.alidns.com/dns-query', // 阿里云公共DNS
+  'https://doh.pub/dns-query', // 腾讯DNSPod
+  'https://doh.360.cn/dns-query', // 360安全DNS
+];
+
+// 国外DNS服务器
+const foreignNameservers = [
+  'https://1.0.0.1/dns-query', // Cloudflare(备)
+  'https://208.67.222.222/dns-query', // OpenDNS(主)
+  'https://208.67.220.220/dns-query', // OpenDNS(备)
+  'https://194.242.2.2/dns-query', // Mullvad(主)
+  'https://194.242.2.3/dns-query', // Mullvad(备)
+];
+
 export default (config: ClashConfig, profileName: string): ClashConfig => {
-  const dns = config.dns ?? {};
-  dns['use-system-hosts'] = true;
-  dns['default-nameserver'] = [
-    'https://223.5.5.5/dns-query',
-    'https://1.12.12.12/dns-query',
-  ];
-  dns.nameserver = [
-    'https://doh.pub/dns-query',
-    'https://dns.alidns.com/dns-query',
-  ];
-  dns['nameserver-policy'] = Object.fromEntries(
-    innerDomain.map((v) => ['+.' + v, ['system']])
-  );
-  dns.fallback = [
-    'https://1.0.0.1/dns-query',
-    'https://208.67.222.222/dns-query',
-    'https://208.67.220.220/dns-query',
-  ];
-  config.dns = dns;
+  config.dns = {
+    ...(config.dns || {}),
+    'use-system-hosts': true,
+    'enhanced-mode': 'fake-ip',
+    'fake-ip-range': '198.18.0.1/16',
+    'fake-ip-filter': [
+      // 本地主机/设备
+      '+.lan',
+      '+.local',
+      // Windows网络出现小地球图标
+      '+.msftconnecttest.com',
+      '+.msftncsi.com',
+      // QQ快速登录检测失败
+      'localhost.ptlogin2.qq.com',
+      'localhost.sec.qq.com',
+      // 微信快速登录检测失败
+      'localhost.work.weixin.qq.com',
+    ],
+    'default-nameserver': [
+      'https://223.5.5.5/dns-query',
+      'https://1.12.12.12/dns-query',
+    ],
+    nameserver: [...domesticNameservers, ...foreignNameservers],
+    'proxy-server-nameserver': [...domesticNameservers, ...foreignNameservers],
+    'nameserver-policy': {
+      ...Object.fromEntries(innerDomain.map((v) => ['+.' + v, ['system']])),
+      'geosite:private,cn,geolocation-cn': domesticNameservers,
+      'geosite:google,youtube,telegram,gfw,geolocation-!cn': foreignNameservers,
+    },
+    fallback: [
+      'https://1.0.0.1/dns-query',
+      'https://208.67.222.222/dns-query',
+      'https://208.67.220.220/dns-query',
+    ],
+  };
 
   const proxies = (config.proxies || []).filter((v) => {
     return removeNames.every((n) => !v.name.includes(n));
